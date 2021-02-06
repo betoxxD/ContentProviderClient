b469a6ea64126f58b88dba5e27b2393eb078a873
marco alberto diosdado nava <beto_100puma70@hotmail.com>
2021-02-06 17:47:36 -0600

    Eliminar y actualizar funcionando. Carga automáticamente los cambios en el recyclerView.

--- app/src/main/java/com/example/clientecontentprovider/EditActivity.java
+++ app/src/main/java/com/example/clientecontentprovider/EditActivity.java
@@ -4,34 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditActivity extends AppCompatActivity {
 
     Button btnAgregar;
+    Button btnEliminar;
     EditText txtName;
     EditText txtPass;
     EditText txtEmail;
     EditText txtTel;
-    int id;
+    long id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
 
-        id = -1;
         btnAgregar = findViewById(R.id.btnAdd);
+        btnEliminar = findViewById(R.id.btnEliminar);
         txtName = findViewById(R.id.txtName);
         txtPass = findViewById(R.id.txtPass);
         txtEmail = findViewById(R.id.txtEmail);
         txtTel = findViewById(R.id.txtTel);
 
+        Intent intent = getIntent();
+        id = intent.getLongExtra("id",-1);
+
+        if(id != -1) {
+            btnAgregar.setText("Modificar");
+            btnEliminar.setVisibility(View.VISIBLE);
+            Usuario usuario = null;
+            Cursor cursor = getContentResolver().query(MiProveedorContenidoContract.Usuarios.CONTENT_URI.buildUpon().appendPath(id+"").build(),null,null,null,null);
+            while (cursor.moveToNext()){
+                usuario = new Usuario(cursor.getInt(0),cursor.getString(1),cursor.getString(2),cursor.getString(3),cursor.getString(4));
+            }
+            txtName.setText(usuario.getName());
+            txtPass.setText(usuario.getPass());
+            txtEmail.setText(usuario.getEmail());
+            txtTel.setText(usuario.getTel());
+        }
+        ContentResolver cr = getContentResolver();
         btnAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,14 +61,28 @@ public class EditActivity extends AppCompatActivity {
                 cv.put(MiProveedorContenidoContract.Usuarios.PASS,txtPass.getText().toString());
                 cv.put(MiProveedorContenidoContract.Usuarios.EMAIL,txtEmail.getText().toString());
                 cv.put(MiProveedorContenidoContract.Usuarios.TELEFONO,txtTel.getText().toString());
-                ContentResolver cr = getContentResolver();
                 if(id == -1){
                     Uri uriResultado = cr.insert(MiProveedorContenidoContract.Usuarios.CONTENT_URI,cv);
                     Log.d("MICP",uriResultado.toString());
+                    finish();
                 }else {
                     String args[] = {id+""};
-                    int uriResultado = cr.update(MiProveedorContenidoContract.Usuarios.CONTENT_URI,cv,MiProveedorContenidoContract.Usuarios._ID + "LIKE ?", args);
+                    int uriResultado = cr.update(MiProveedorContenidoContract.Usuarios.CONTENT_URI.buildUpon().appendPath(id+"").build(),cv,null,null);
                     Log.d("MICP",uriResultado+"");
+                    finish();
+                }
+            }
+        });
+
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int uriResultado = cr.delete(MiProveedorContenidoContract.Usuarios.CONTENT_URI.buildUpon().appendPath(id+"").build(),null,null);
+                if(uriResultado == 1) {
+                    Toast.makeText(EditActivity.this, "Eliminado correctamente.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }else {
+                    Toast.makeText(EditActivity.this, "No se pudo eliminar.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
--- app/src/main/java/com/example/clientecontentprovider/MainActivity.java
+++ app/src/main/java/com/example/clientecontentprovider/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton btnAdd;
     RecyclerView recyclerView;
+    Vector<Usuario> usuarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,33 +41,34 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        chargeRecyclerView();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        chargeRecyclerView();
+    }
+
+    private void chargeRecyclerView() {
         // Inicializar reciclerView
-        Vector<Usuario> usuarios = new Vector<>();
+        usuarios = new Vector<>();
         recyclerView = findViewById(R.id.rvMain);
         GridLayoutManager layoutManager = new GridLayoutManager(this,2);
         recyclerView.setLayoutManager(layoutManager);
-
         Cursor cursor = getContentResolver().query(MiProveedorContenidoContract.Usuarios.CONTENT_URI,null,null,null,null);
         while (cursor.moveToNext()){
-            Usuario usuario = new Usuario(cursor.getInt(0),cursor.getString(1),cursor.getString(2),cursor.getString(3),cursor.getString(4));
+            Usuario usuario = new Usuario(cursor.getInt(0),cursor.getString(1),cursor.getString(3),cursor.getString(2),cursor.getString(4));
             usuarios.add(usuario);
-            // Log.d("MICP",cursor.getInt(0) +"-"+ cursor.getString(2));
         }
         ElementAdapter elementAdapter = new ElementAdapter(this, usuarios);
         elementAdapter.setOnclickListener(
                 v1 -> {
-                    Toast.makeText(this, "Elemento seleccionado: " + recyclerView.getChildAdapterPosition(v1) , Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this,EditActivity.class);
+                    intent.putExtra("id",usuarios.get(recyclerView.getChildAdapterPosition(v1)).getId());
+                    startActivity(intent);
                 }
         );
         recyclerView.setAdapter(elementAdapter);
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
-
-
-        return super.onCreateView(name, context, attrs);
     }
 }
